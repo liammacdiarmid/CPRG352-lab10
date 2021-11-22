18842e7206664fdce00f2f9773ef7c61059ad970
liammacdiarmid <81202661+liammacdiarmid@users.noreply.github.com>
2021-11-21 21:40:24 -0700

    Add files via upload

---
+++ src/java/filters/AuthenticationFilter.java
@@ -0,0 +1,44 @@
+package filters;
+
+import java.io.IOException;
+import java.io.PrintStream;
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+public class AuthenticationFilter implements Filter {
+
+  @Override
+  public void doFilter(
+    ServletRequest request,
+    ServletResponse response,
+    FilterChain chain
+  )
+    throws IOException, ServletException {
+    HttpServletRequest httpReq = (HttpServletRequest) request;
+    HttpSession session = httpReq.getSession();
+    String email = (String) session.getAttribute("email");
+
+    if (email == null) {
+      HttpServletResponse httpRes = (HttpServletResponse) response;
+      httpRes.sendRedirect("login");
+      return;
+    }
+
+    chain.doFilter(request, response);
+  }
+
+  @Override
+  public void init(FilterConfig filterConfig) throws ServletException {}
+
+  @Override
+  public void destroy() {}
+}
